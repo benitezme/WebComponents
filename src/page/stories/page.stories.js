149a3c4758ef7fdff7d361589d0ba921ea4b6691
Barry Low <barrylow@gmail.com>
2018-12-04 21:22:11 -0800

    Add page component

---
+++ src/page/stories/page.stories.js
@@ -0,0 +1,11 @@
+/* eslint react/prop-types: 0 */
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { withKnobs, text } from '@storybook/addon-knobs'
+
+import MessageCard from '../index'
+
+storiesOf('MessageCard', module)
+  .addDecorator(withKnobs)
+  .add('with text', () => <MessageCard message={text('Message', 'This is a message')} />)
